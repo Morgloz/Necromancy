9c8c86d284bfc4049123add5e4a4ad704a517950
Morgloz <alejandromvlm@gmail.com>
2016-11-18 01:52:34 +0100

    Start

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Necromancy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Necromancy mod for RimWorld")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Necromancy")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Morgloz 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ WorkGiver_Necromancer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Necromancy
+{
+    public class WorkGiver_Necromancer : WorkGiver_Scanner
+    {
+        private readonly JobDef jobDef = DefDatabase<JobDef>.GetNamed("RaiseDead");
+
+        public override PathEndMode PathEndMode
+        {
+            get
+            {
+                return PathEndMode.OnCell;
+            }
+        }
+
+        public override bool HasJobOnThing(Pawn pawn, Thing t)
+        {
+            var corpse = t as Corpse;
+            return true;
+        }
+
+        public override Job JobOnThing(Pawn pawn, Thing t)
+        {
+            return new Job(jobDef, t);
+        }
+    }
+}
